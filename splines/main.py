8828d9eb67436958a4ea42628c31258bb7b60a11
eklerka72 <114610149+eklerka72@users.noreply.github.com>
2023-12-11 17:48:38 +0300

    Add files via upload

--- splines/main.py
+++ splines/main.py
@@ -3,9 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def evaluate_function(function, point):
-    return eval(str(function) + "(" + str(point) + ")")
-
 def progonka(A):
     n = len(A) - 1
 
@@ -23,65 +20,51 @@ def progonka(A):
     x[n] = (A[n][n - 1] * Q[n - 1] - A[n][-1]) / (-A[n][n] - A[n][n - 1] * P[n - 1])
     for i in range(n - 1, -1, -1):
         x[i] = round(P[i] * x[i + 1] + Q[i], 5)
+    print(P)
+    print(Q)
     return(x)
 
+x = [0, 1, 2, 3, 4]
+I = [-0.6666666667, 7.3333333333, 44.3333333333, 159.363]
+n = len(x) - 1
+h = x[1] - x[0]
+A = [[0] * (n + 2) for i in range(n + 1)]
+print(A)
+for i in range(1, n):
+    A[i][i - 1] = 1/h
+    A[i][i] = 4/h
+    A[i][i+1] = 1/h
+    A[i][-1] = 3*(I[i-1] + I[i])
+A[0][0] = 1
+A[0][1] = 2
+A[0][-1] = (5/h**2*I[0]+1/h**2*I[1])/2
+A[n][n] = 1
+A[n][n-1] = 2
+A[n][-1] = (5/h**2*I[3]+1/h**2*I[2])/2
+for a in A:
+    print(a)
+m = progonka(A)
+print(m)
+S = [0] * n
+"""for i in range(n):
+    S[i] = f[i] + np.poly1d([x[i]], True) * (1/h[i+1]*(f[i+1]-f[i]) - h[i+1]/2*m[i] - h[i+1]/6*(m[i+1]-m[i])) + m[i]/2*np.poly1d([x[i], x[i]], True) + 1/(6*h[i+1])*(m[i+1] - m[i])*np.poly1d([x[i], x[i], x[i]], True)"""
+for i in range(n):
+    S[i] = np.poly1d([-6/h**3*(I[i]-h*m[i]) + 3/h**2*(m[i+1] - m[i]), 6/h**2*(I[i]-h*m[i]) - 2/h*(m[i+1] - m[i]), m[i]])
+print(S)
+for i in range(n):
+   print(f"{S[i][0]} + {S[i][0 + 1]} * (x - {x[i]}) + {S[i][0 + 2]} * (x - {x[i]})^2")
 
+plt.title("Интерполяционный кубический сплайн")
+x_interp = np.linspace(np.min(x), np.max(x), 5000)
+#plt.plot(x, I, "o", label="Data points")
+for i in range(len(x)-1):
+    xi = np.linspace(x[i], x[i + 1])
+    plt.plot(xi, I[i], "red", label="First line")
 
-def cubic_splain(x, f):
-    n = len(x) - 1
-    #f = []
-    h = []
-    m = [0] * (n + 1)
-    for i in range(n + 1):
-        # fi = evaluate_function(y, x[i])
-        # f.append(round(fi, 7))
-        if i != n:
-            h.append(round(x[i + 1] - x[i], 7))
-    h.append(h[-1])
-    flag = 1
-    for i in range(n - 1):
-        if h[i] != h[i + 1]:
-            flag = 0
-            break
-    if flag:
-        m[0] = round((2 * f[0] - 5 * f[1] + 4 * f[2] - f[3]) / (h[0] ** 2), 7)
-        m[n] = round((-f[n - 3] + 4 * f[n - 2] - 5 * f[n - 1] + 2 * f[n]) / (h[0] ** 2), 7)
-        A = mas = [[0] * (n + 2) for i in range(n + 1)]
-        for i in range(1, n):
-            A[i][i - 1] = h[i - 1] / 6
-            A[i][i] = (h[i - 1] + h[i]) / 3
-            A[i][i + 1] = h[i] / 6
-            A[i][-1] = (f[i + 1] - f[i]) / h[i] - (f[i] - f[i - 1]) / h[i - 1]
-        A[0][0] = 1
-        A[0][-1] = m[0]
-        A[n][n] = 1
-        A[n][-1] = m[n]
-        m = progonka(A)
-    S = [0] * n
-    """for i in range(n):
-        S[i] = f[i] + np.poly1d([x[i]], True) * (1/h[i+1]*(f[i+1]-f[i]) - h[i+1]/2*m[i] - h[i+1]/6*(m[i+1]-m[i])) + m[i]/2*np.poly1d([x[i], x[i]], True) + 1/(6*h[i+1])*(m[i+1] - m[i])*np.poly1d([x[i], x[i], x[i]], True)"""
-    for i in range(n):
-        S[i] = np.poly1d([1 / (6 * h[i + 1]) * (m[i + 1] - m[i]), m[i] / 2,
-                          (1 / h[i + 1] * (f[i + 1] - f[i]) - h[i + 1] / 2 * m[i] - h[i + 1] / 6 * (m[i + 1] - m[i])),
-                          f[i]])
-    print(S)
-    for i in range(n):
-        print(
-            f"{S[i][0]} + {S[i][0 + 1]} * (x - {x[i]}) + {S[i][0 + 2]} * (x - {x[i]})^2 + {S[i][0 + 3]} * (x - {x[i]})^3")
+for i in range(len(S)):
+     t = np.linspace(x[i], x[i + 1], 1000)
+     plt.plot(t, S[i](t - x[i]), "green")
+plt.plot(0, 0, "green", label="Cubic splain")
 
-    plt.title("Интерполяционный кубический сплайн")
-    x_interp = np.linspace(np.min(x), np.max(x), 5000)
-    plt.plot(x, f, "o", label="Data points")
-    plt.plot(x, f, "red", label="First line")
-
-    for i in range(len(S)):
-        t = np.linspace(x[i], x[i + 1], 1000)
-        plt.plot(t, S[i](t - x[i]), "green")
-    plt.plot(0, 0, "green", label="Cubic splain")
-
-    plt.legend()
-    plt.show()
-
-f = np.array([1, math.sqrt(3)/2, 1/2, 0])
-x = np.array([0, math.pi/6, math.pi/3, math.pi/2])
-cubic_splain(x, f)
+plt.legend()
+plt.show()
